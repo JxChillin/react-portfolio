8d3bc0609fa0964f304e967d815082396387ae91
JxChillin <Jaylend10@gmail.com>
2023-11-01 12:42:05 -0400

    creating actual components

---
+++ src/app/header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MyHeader() {
+  return (
+    <div className="bg-[rgba(_0,0,0,0.8)] w-fit p-6 rounded-[15px] ">
+      <p className="text-[40px] p-2 text-white Arial">
+        {/* Software Engineer */}
+        <h1 className="text">
+          Hi, I'm <span>Jaylen</span>
+          <br />
+          <br />
+          from North Carolina
+        </h1>
+      </p>
+    </div>
+  );
+}
+
+export default MyHeader;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Head from "next/head";
 import headerimage from "@/app/images/nightraleigh.png";
-
+import MyHeader from "./header";
 export default function Page() {
   return (
     <head>
@@ -11,13 +11,8 @@ export default function Page() {
         <div className="w-full h-screen bg-[url(images/nightraleigh.png)] bg-cover bg-center">
           {/* Header Container */}
           <div className="px-[10%] py-[100px]">
-            <div className="text-base mt-[30%] color-white ">
-              <div className="bg-[rgba(_0,0,0,0.8)] w-fit p-6 rounded-[15px] ">
-                <p className="text-[60px] text-white">
-                  Test Text
-                  {/* <h1></h1> */}
-                </p>
-              </div>
+            <div className="text-base mt-[20%] ml-[10%] color-white ">
+              <MyHeader />
             </div>
           </div>
         </div>
