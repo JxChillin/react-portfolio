1a69ea165f9ac76fb1f7a02d844521525d7a996d
JxChillin <jaylend10@gmail.com>
2023-11-07 23:07:00 -0500

    added previous files

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
-import Image from 'next/image'
+import Image from "next/image";
 import Header from "./components/header";
-
+import Head from "next/head";
 
 export default function Home() {
   return (
-<main>
-<div className="w-full h-screen bg-dark-gray bg-cover bg-center">
-          {/* Header Container */}
-          <div className="px-[10%] py-[100px]">
-            <div className=" mt-[20%]">
-              {/* <Header /> */}
-              <Header />
-              {/* <Header /> */}
-            </div>
+    <main>
+      <Head>
+        <title>Jay.Dav</title>
+      </Head>
+      <div className="w-full h-screen bg-dark-gray bg-cover bg-center">
+        {/* Header Container */}
+        <div className="px-[10%] py-[100px]">
+          <div className=" mt-[20%]">
+            <Header />
           </div>
         </div>
-        <div id="About">{/* Section Start */}</div>
-        <div id="Contact">{/* Section Start */}</div>
+      </div>
+      <div id="About">{/* Section Start */}</div>
+      <div id="Contact">{/* Section Start */}</div>
     </main>
-  )
+  );
 }
---
+++ app/pages/about.tsx
@@ -0,0 +1,10 @@
+import NavbarFixed from "../components/navbarfixed";
+
+export default function About() {
+  return (
+    <div>
+      <NavbarFixed />
+      <div className="">About Page</div>
+    </div>
+  );
+}
