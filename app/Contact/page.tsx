c915ab291f321509ad1565257bb54fb874863842
JxChillin <Jaylend10@gmail.com>
2023-11-10 09:06:09 -0500

    Switching to NextJS Framework

--- app/Contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import NavbarFixed from "../components/navbarfixed";
-
-export default function Page() {
-  return (
-    <div>
-      <div className="text-white">Contact Page</div>
-    </div>
-  );
-}
